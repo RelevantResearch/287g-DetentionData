a15fe8bb7c123116bf8b1c9b3e09bbc04f7883d7
bishal784411 <timsinabishal232@gmail.com>
2025-05-01 14:08:46 +0545

    New Sheets Alert! participatingAgencies04302025pm.xlsx and pendingAgencies04302025pm.xlsx

---
+++ Monitor/messageToSlack.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the Slack API token and channel ID from the environment
+SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
+SLACK_CHANNEL_ID = os.getenv('SLACK_CHANNEL_ID')
+
+def send_message_to_slack(message):
+    url = "https://slack.com/api/chat.postMessage"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {SLACK_API_TOKEN}"
+    }
+
+    payload = {
+        "channel": SLACK_CHANNEL_ID,
+        "text": message
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200 and response.json().get("ok"):
+        print("Message sent successfully to Slack.")
+    else:
+        print(f"Error sending message to Slack: {response.text}")
+
+if __name__ == "__main__":
+    message = "The latest files are ready for GitHub push!"
+    send_message_to_slack(message)
